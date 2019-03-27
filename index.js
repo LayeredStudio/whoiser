7eb4ab0c6c368fe8ee4be7eb7c78565ec243a17a
Andrei Igna <andrei.igna@layered.studio>
2019-03-27 18:28:28 +0100

    Fixed - allow 1 char domain parts

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const isDomain = domain => {
 	}
 
 	const labels = punycode.toASCII(domain).split('.').reverse()
-	const labelTest = /^([a-z0-9-]{2,64}|xn[a-z0-9-]{5,})$/i
+	const labelTest = /^([a-z0-9-]{1,64}|xn[a-z0-9-]{5,})$/i
 
 	return labels.length > 1 && labels.every((label, index) => {
 		return index ? labelTest.test(label) && !label.startsWith('-') && !label.endsWith('-') : isTld(label)
