426a904750f044502c6a611407c722c766f6c622
Andrei <andrei@laye.red>
2019-07-19 22:23:58 +0200

    Updated - separate the util functions to own file

--- index.js
+++ index.js
@@ -2,39 +2,7 @@ const net = require('net')
 const https = require('https')
 const url = require('url')
 const punycode = require('punycode')
-
-const splitStringBy = (string, by) => [string.slice(0, by), string.slice(by + 1)]
-const requestGetBody = url => {
-	return new Promise((resolve, reject) => {
-		https.get(url, resp => {
-			let data = '';
-			resp.on('data', chunk => data += chunk);
-			resp.on('end', () => resolve(data));
-			resp.on('error', reject);
-		}).on('error', reject);
-	});
-}
-
-const isTld = tld => {
-	if (tld.startsWith('.')) {
-		tld = tld.substring(1)
-	}
-
-	return /^([a-z]{2,64}|xn[a-z0-9-]{5,})$/i.test(punycode.toASCII(tld))
-}
-
-const isDomain = domain => {
-	if (domain.endsWith('.')) {
-		domain = domain.substring(0, domain.length - 1);
-	}
-
-	const labels = punycode.toASCII(domain).split('.').reverse()
-	const labelTest = /^([a-z0-9-]{1,64}|xn[a-z0-9-]{5,})$/i
-
-	return labels.length > 1 && labels.every((label, index) => {
-		return index ? labelTest.test(label) && !label.startsWith('-') && !label.endsWith('-') : isTld(label)
-	})
-}
+const { splitStringBy, requestGetBody, isTld, isDomain } = require('./utils.js')
 
 // cache
 let cacheTldWhoisServer = {
---
+++ utils.js
@@ -0,0 +1,40 @@
+const punycode = require('punycode')
+const splitStringBy = (string, by) => [string.slice(0, by), string.slice(by + 1)]
+
+
+const requestGetBody = url => {
+	return new Promise((resolve, reject) => {
+		https.get(url, resp => {
+			let data = ''
+			resp.on('data', chunk => data += chunk)
+			resp.on('end', () => resolve(data))
+			resp.on('error', reject)
+		}).on('error', reject)
+	})
+}
+
+const isTld = tld => {
+	if (tld.startsWith('.')) {
+		tld = tld.substring(1)
+	}
+
+	return /^([a-z]{2,64}|xn[a-z0-9-]{5,})$/i.test(punycode.toASCII(tld))
+}
+
+const isDomain = domain => {
+	if (domain.endsWith('.')) {
+		domain = domain.substring(0, domain.length - 1)
+	}
+
+	const labels = punycode.toASCII(domain).split('.').reverse()
+	const labelTest = /^([a-z0-9-]{1,64}|xn[a-z0-9-]{5,})$/i
+
+	return labels.length > 1 && labels.every((label, index) => {
+		return index ? labelTest.test(label) && !label.startsWith('-') && !label.endsWith('-') : isTld(label)
+	})
+}
+
+module.exports.splitStringBy = splitStringBy
+module.exports.requestGetBody = requestGetBody
+module.exports.isTld = isTld
+module.exports.isDomain = isDomain
