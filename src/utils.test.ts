054308f04af5a2b3b327b08df98051db82fd8a96
Andrei <andrei@laye.red>
2025-08-16 12:53:50 +0200

    updates: tests for utils

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,10 +1,29 @@
 import { strict as assert } from 'node:assert'
 import { suite, test } from 'node:test'
 
-import { validatedTld } from './utils.ts'
+import { isDomain, validatedTld } from './utils.ts'
+
+suite('isDomain()', () => {
+	test('invalid domains', function () {
+		assert.equal(false, isDomain(''), 'empty')
+		assert.equal(false, isDomain('c'), 'too short')
+		assert.equal(false, isDomain('c om'), 'contains space')
+		assert.equal(false, isDomain('domain'), 'no tld')
+		assert.equal(false, isDomain('-example.com'), 'starts with hyphen')
+		assert.equal(false, isDomain('example.com1'), 'tld contains number')
+		assert.throws(() => validatedTld('clearlyinvalidtldbecasuethisistoooooooooooooloooooooooooooooooong.com'), 'too long')
+	})
+
+	test('valid domain string', function () {
+		assert.ok(isDomain('example.com'))
+		assert.ok(isDomain('.example.com.'))
+		assert.ok(isDomain('youtu.be'))
+		assert.ok(isDomain('blog.google'))
+	})
+})
 
 suite('validatedTld()', () => {
-	test('invalid TLDs', function() {
+	test('invalid TLDs', function () {
 		assert.throws(() => validatedTld(''), 'empty')
 		assert.throws(() => validatedTld('c'), 'too short')
 		assert.throws(() => validatedTld('c om'), 'contains space')
@@ -15,18 +34,18 @@ suite('validatedTld()', () => {
 		assert.throws(() => validatedTld('clearlyinvalidtldbecasuethisistoooooooooooooloooooooooooooooooong'), 'too long')
 	})
 
-	test('valid - TLDs', function() {
+	test('valid - TLDs', function () {
 		assert.equal(validatedTld('.com'), 'com')
 		assert.equal(validatedTld('org.'), 'org')
 		assert.equal(validatedTld('AI'), 'ai')
 		assert.equal(validatedTld('.nyc'), 'nyc')
 	})
 
-	test('valid - SLDs', function() {
+	test('valid - SLDs', function () {
 		assert.equal(validatedTld('.co.uk'), 'co.uk')
 	})
 
-	test('valid - IDN', function() {
+	test('valid - IDN', function () {
 		assert.equal(validatedTld('xn--zckzah'), 'xn--zckzah')
 		assert.equal(validatedTld('テスト'), 'テスト')
 		assert.equal(validatedTld('.香港'), '香港')
--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,20 @@
 import { toASCII } from 'punycode-esm'
 
+/**
+ * Split a string into two parts at a given index.
+ *
+ * @param string The string to split.
+ * @param by The index at which to split the string.
+ * @returns An array containing the two parts of the string.
+ */
 export function splitStringBy(string: string, by: number): [string, string] {
 	return [string.slice(0, by), string.slice(by + 1)]
 }
 
 /**
  * Check if a string is a valid TLD, and return it in canonical form.
- * 
- * @param tld 
+ *
+ * @param tld
  * @returns The normalized TLD
  * @throws If the TLD is invalid
  */
@@ -23,9 +30,9 @@ export function validatedTld(tld: string): string {
 	}
 
 	const labelTest = /^([a-z]{2,64}|xn[a-z0-9-]{5,})$/
-	const labels = tld.split('.').map(label => toASCII(label))
+	const labels = tld.split('.').map((label) => toASCII(label))
 
-	if (!labels.every(label => labelTest.test(label))) {
+	if (!labels.every((label) => labelTest.test(label))) {
 		throw new Error(`Invalid TLD "${tld}"`)
 	}
 
@@ -33,7 +40,10 @@ export function validatedTld(tld: string): string {
 	return tld
 }
 
-export function isTld(tld: string): boolean {
+/**
+ * Check if a string is a valid TLD format
+ */
+function isTld(tld: string): boolean {
 	if (tld.startsWith('.')) {
 		tld = tld.substring(1)
 	}
@@ -42,9 +52,17 @@ export function isTld(tld: string): boolean {
 	return /^([a-z]{2,64}|xn[a-z0-9-]{5,})$/i.test(toASCII(tld))
 }
 
+/**
+ * Check if a string is a valid domain format
+ * @param domain The domain to check.
+ */
 export function isDomain(domain: string): boolean {
+	if (domain.startsWith('.')) {
+		domain = domain.slice(1)
+	}
+
 	if (domain.endsWith('.')) {
-		domain = domain.substring(0, domain.length - 1)
+		domain = domain.slice(0, -1)
 	}
 
 	const labels = toASCII(domain).split('.').reverse()
